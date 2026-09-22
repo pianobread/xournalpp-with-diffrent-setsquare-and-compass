029254d691dad23f9c206346c924c6209be10761
star gem <240804331+pianobread@users.noreply.github.com>
2026-09-23 01:51:35 +0300

    done reverting

--- src/core/view/CompassView.cpp
+++ src/core/view/CompassView.cpp
@@ -120,9 +120,7 @@ void CompassView::drawAngularMarks(cairo_t* cr) const {
         const double tick = (i % 5 == 0) ? TICK_LARGE : TICK_SMALL;
         cairo_move_to(cr, this->height * cs, this->height * si);
         if (i % angularCaptionOffset == 0) {
-            const double radTickEnd = (i == 270)
-                                          ? (this->circlePos + 1.5 * CIRCLE_RAD)
-                                          : (angularCaptionPos + 0.3);
+            const double radTickEnd = (i == 270) ? (this->circlePos + 1.5 * CIRCLE_RAD) : (angularCaptionPos + 0.3);
             cairo_line_to(cr, radTickEnd * cs, radTickEnd * si);
             cairo_move_to(cr, angularCaptionPos * cs, angularCaptionPos * si);
             showTextCenteredAndRotated(cr, std::to_string(360 - i), i + 90);
