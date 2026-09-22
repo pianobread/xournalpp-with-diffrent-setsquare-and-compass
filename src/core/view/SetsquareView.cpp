deae468de122e51275dfa211d584004fa43835e7
star gem <240804331+pianobread@users.noreply.github.com>
2026-09-23 01:39:16 +0300

    reverted everything except the color change to black
    Added vertical marks within semicircle and updated clipping.

--- src/core/view/SetsquareView.cpp
+++ src/core/view/SetsquareView.cpp
@@ -90,6 +90,7 @@ void SetsquareView::drawGeometryTool(cairo_t* cr) const {
     cairo_close_path(cr);
     cairo_clip(cr);
 
+    // Keep everything black
     cairo_set_source_rgb(cr, 0., 0., 0.);
     drawOutline(cr);
     drawHorizontalMarks(cr);
@@ -201,7 +202,20 @@ void SetsquareView::drawAngularMarks(cairo_t* cr) const {
 
 void SetsquareView::drawVerticalMarks(cairo_t* cr) const {
     xoj::util::CairoSaveGuard saveGuard(cr);
+    const auto max = this->maxVmark / 10;  // number of full centimeters
 
+    // BEGIN: VERTICAL marks within semicircle
+    clipVerticalStripes(cr);
+
+    // draw vertical marks
+    for (double i = .5; i <= max; i += .5) {
+        const double x = cathete(this->radius - .25, i);
+        cairo_move_to(cr, -x, i);
+        cairo_line_to(cr, x, i);
+        cairo_stroke(cr);
+    }
+    cairo_reset_clip();
+    // END: VERTICAL marks within circle
 
     // BEGIN: vertical measuring marks with numbers
     for (int i = this->minVmark; i <= this->maxVmark; i++) {
